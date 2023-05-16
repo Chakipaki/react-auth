3ac552b014727b501a26a27ebf533826ce6862fd
Ivan Bieliaiev <belyaeve2@icloud.com>
2023-05-16 18:07:28 +0200

    Auth action

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,7 +1,47 @@
 import AuthForm from '../components/AuthForm';
+import {json, redirect} from "react-router-dom";
 
 function AuthenticationPage() {
     return <AuthForm/>;
 }
 
 export default AuthenticationPage;
+
+export const action = async ({request, params}) => {
+    const searchParams = new URL(request.url).searchParams;
+    const mode = searchParams.get('mode') || 'login';
+
+    if (mode !== 'login' && mode !== 'signup') {
+        throw json({ message: 'Unsupported mode' }, {
+            status: 422
+        });
+    }
+
+    const data = await request.formData();
+    const authData = {
+        email: data.get('email'),
+        password: data.get('password')
+    };
+
+    const response = await fetch('http://localhost:8080/' + mode, {
+        method: request.method,
+        headers: {
+            'Content-Type': 'application/JSON'
+        },
+        body: JSON.stringify(authData)
+    });
+
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    }
+
+    if (response.ok === false) {
+        throw json({ message: 'Could not auth user!' }, {
+            status: 500
+        });
+    }
+
+    // Later: Manage token
+
+    return redirect('/');
+}
